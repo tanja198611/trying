75d67e01d5d193345e58a99839edcd9671a86ebb
Zenja <johnpoliwkin@gmail.com>
2023-12-10 14:29:07 +0300

    13msk

--- index.php
+++ index.php
@@ -2,9 +2,5 @@
 
 
 
+echo "another first switch state 13msk";
 
-<<<<<<< HEAD
-echo "another first switch cee";
-=======
-echo "another first switch dee";
->>>>>>> af4de2e663794c2eef8d81957c234ca8a8ae3f26
